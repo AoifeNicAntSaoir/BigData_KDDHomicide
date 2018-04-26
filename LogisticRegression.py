cca8468d80833fcdf3175c6d0d144656c3a8bfd3
Aoife Sayers <aoifesayers@gmail.com>
2018-04-26 14:18:39 +0100

    Better Logistic Reg Res to match Journal Result

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,25 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 25 22:19:38 2018
-
-@author: Aoife Sayers
-
-"""
-#Tutorial link: https://towardsdatascience.com/train-test-split-and-cross-validation-in-python-80b61beca4b6
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.feature_selection import RFE
+#importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+#loading dataset and separating predictors from predicted variable
 df = pd.read_csv('database.csv', low_memory=False)
 
-df = df[(df['Crime Solved']=='Yes') & (df['Perpetrator Race']!='Unknown')]
-df = df[['Perpetrator Sex','Perpetrator Race']]
+df = df[(df['Crime Solved']=='Yes') & (df['Perpetrator Race']!='Unknown') & (df['Victim Sex']!='Unknown')]
 
-y=df['Perpetrator Sex']
-df = pd.get_dummies(df, columns=['Perpetrator Sex','Perpetrator Race'])
+X = df[['Year', 'Victim Ethnicity', 'Victim Sex', 'Victim Age', 'Victim Race']]
 
-X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.3)
+X = pd.get_dummies(X)
+
+Y = df['Perpetrator Sex']
+#Y = pd.get_dummies(Y)
+
+X_train, X_test, y_train, y_test = train_test_split(X,Y, test_size=0.3)
 
 
 lr = LogisticRegression()
@@ -27,3 +24,5 @@ lr.fit(X_train, y_train)
 predictions = lr.predict(X_test)
 print(predictions)
 print(predictions[0:5])
+print('Score:', lr.score(X_test, y_test))
+
